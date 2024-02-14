4ed5779b23673c5d02db2b2b0d6b1cd95b0bc874
HMaroba <marobahlalele910@gmail.com>
2024-02-14 09:07:22 +0200

    Initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Lebelo Documentation</span>,
+  project: {
+    link: "https://github.com/HMaroba",
+  },
+  chat: {
+    link: "https://discord.com",
+  },
+};
+
+export default config;
